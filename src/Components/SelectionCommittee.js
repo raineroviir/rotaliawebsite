f31b3924b95062984f2d2f6880cde9ac4999fc41
Rainer <roviir@gmail.com>
2017-02-28 18:01:45 -0800

    typed up bylaws and fixed home page style, and pushed header down to match logo on left

--- src/Components/SelectionCommittee.js
+++ src/Components/SelectionCommittee.js
@@ -3,15 +3,25 @@ import React from 'react'
 export default class SelectionCommittee extends React.Component {
   render() {
     return (
-      <div>
-        <p>Rein Grabbi, MS, Committee Chairman, Tallinn, Estonia</p>
-        <p>Sirje Mark, Executive Administrator, Tartu University, Tartu, Estonia</p>
-        <p>Peeter Järvalaid, Law Professor, Tallinn University, Tallinn, Estonia</p>
-        <p>Rein Laaneots, Emeritus Professor, Tallinn Technical University, Tallinn, Estonia</p>
-        <p>Indrek Pajumaa, Corporation Rotalia and Mercuri Urval OŰ, Tallinn, Estonia</p>
-        <p>Hardi Tullus, Professor, Estonian Agricultural University, Tartu, Estonia</p>
-        <p>Peeter Tulviste, Former Rector of Tartu University and Member of Estonia's</p>
-        <p>Parliament, Tartu/Tallinn, Estonia</p>
+      <div><p>Since the Foundation is based in Seattle, Washington, USA, and most of the scholarship recipients reside in Estonia, therefore, the Foundation formed a Scholarship Application Review Committee in Estonia.  The Committee is made up of professors  and administrators of Tartu University, Tallinn University, Tallinn Technical University, Estonian Agricultural University,  Art Institute, and Music and Theatrical Institute, and Corporation Rotalia.  The Committee is Chaired by Rein Grabbi, residing in Estonia and assisted by Sirje Mark, Executive Administrator, University of Tartu.  The current Scholarship Selection Committee consists of the following:</p>
+      <ul>
+      <li>Rein Grabbi, MS, Committee Chairman, Tallinn, Estonia
+      </li>
+      <li>Sirje Mark, Executive Administrator, Tartu University, Tartu, Estonia
+      </li>
+      <li>Peeter Järvalaid, Law Professor, Tallinn University, Tallinn, Estonia
+      </li>
+      <li>Rein Laaneots, Emeritus Professor, Tallinn Technical University, Tallinn, Estonia
+      </li>
+      <li>Indrek Pajumaa, Corporation Rotalia and Mercuri Urval OŰ, Tallinn, Estonia
+      </li>
+      <li>Hardi Tullus, Professor, Estonian Agricultural University, Tartu, Estonia
+      </li>
+      <li>Peeter Tulviste, Former Rector of Tartu University and Member of Estonia's
+      </li>
+      <li>Parliament, Tartu/Tallinn, Estonia
+      </li>
+      </ul>
       </div>
     )
   }
